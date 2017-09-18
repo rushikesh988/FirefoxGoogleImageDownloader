86f812066adc7dbce84dd77f9b76dc504de15b81
Rushikesh Tade <rushtade@in.ibm.com>
2017-09-18 16:10:48 +0530

    Working and optimized some code

--- downloader.js
+++ downloader.js
@@ -1,9 +1,4 @@
 (function() {
-  /*
-  Check and set a global guard variable.
-  If this content script is injected into the same page again,
-  it will do nothing next time.
-  */
   if (window.hasRun) {
     return;
   }
@@ -14,11 +9,9 @@ var docTitle=document.title;
 titleAfterRemovingDash=docTitle.split("-")
 var imgs=document.getElementsByTagName("a");
 var i=0;
-// var divv= document.createElement("div");
 var aray=new Array();var j=-1;
 while(++i<imgs.length){
     if(imgs[i].href.indexOf("/imgres?imgurl=http")>0){
-      // divv.appendChild(document.createElement("br"));
       aray[++j]=decodeURIComponent(imgs[i].href).split(/=|%|&/)[1].split("?imgref")[0];
       var urlToSave=aray[j].split("/");
       console.log(aray[j])
--- gettitle.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var docTitle=document.title;
-  titleAfterRemovingDash=docTitle.split("-")
-  console.log("Title is inside get Title" + titleAfterRemovingDash);
-  return titleAfterRemovingDash[0];
-  })();
