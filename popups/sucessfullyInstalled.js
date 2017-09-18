4e6906820e324d98c6b5c10e2e2463abcde415b5
Rushikesh Tade <rushtade@in.ibm.com>
2017-09-18 10:37:08 +0530

    Cleaned some code

--- popups/sucessfullyInstalled.js
+++ popups/sucessfullyInstalled.js
@@ -1,65 +1,37 @@
+function onLinksReceived(downloadLinks) {
+  // console.log(`Started downloading: ${downloadLinks}`);
+  var i=0;
+  while(++i<downloadLinks[0].length){
+    
+      console.log(downloadLinks[0][i])
+      var downloading = browser.downloads.download({
+        url : downloadLinks[0][i],
+        conflictAction : 'uniquify'
+      });
+    }
+ }
+  
+function onLinksFailed(error) {
+  console.log(`Download failed: ${error}`);
+}
 
-/*
-Listen for clicks in the popup.
-
-If the click is on one of the beasts:
-  Inject the "beastify.js" content script in the active tab.
-
-  Then get the active tab and send "beastify.js" a message
-  containing the URL to the chosen beast's image.
-
-If it's on a button wich contains class "clear":
-  Reload the page.
-  Close the popup. This is needed, as the content script malfunctions after page reloads.
-*/
 document.addEventListener("click", (e) => {
   
   console.log("in on Click Listner")
   if (e.target.classList.contains("beast")) {
     var chosenBeast = e.target.textContent;
-    console.log("Going for execution of script")
-    browser.tabs.executeScript(null, {
-      file: "../imageDownloader.js"
-    });
-    console.log("Script Executed")
     console.log('====================================');
-    console.log("Rushikesh Its Started");
-    // var getting = browser.tabs.get(
-    //   0              // integer
-    // )
-
-    browser.tabs.executeScript({
+   var aray=browser.tabs.executeScript({
       file: "../downloader.js"
     });
 
-    // var gettingCurrent = browser.tabs.getCurrent();
-    // gettingCurrent.then(onGot, onError);
-
-    // var cont=document.getElementsByTagName("body")[0];
-    // var imgs=document.getElementsByTagName("a");
-    // var i=0;var divv= document.createElement("div");
-    // var aray=new Array();var j=-1;
-    // while(++i<imgs.length){
-    //     if(imgs[i].href.indexOf("/imgres?imgurl=http")>0){
-    //       // divv.appendChild(document.createElement("br"));
-    //       aray[++j]=decodeURIComponent(imgs[i].href).split(/=|%|&/)[1].split("?imgref")[0];
-    //       // divv.appendChild(document.createTextNode(aray[j]));
-    //       console.log(aray[j])
-    //     }
-    // }
-    // cont.insertBefore(divv,cont.childNodes[0]);
-
-
-
-
-    console.log('====================================');
-    
-
-  }
+    aray.then(onLinksReceived, onLinksFailed);
+    console.log(aray);
+     console.log('====================================');
+    }
   else if (e.target.classList.contains("clear")) {
     browser.tabs.reload();
     window.close();
-
     return;
   }
 });
