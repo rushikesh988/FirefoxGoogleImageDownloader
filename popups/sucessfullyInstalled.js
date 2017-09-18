d8ea1187e653d619ddee55ce35abc69ae0f20c68
Rushikesh Tade <rushtade@in.ibm.com>
2017-09-18 12:00:20 +0530

    Working Sucessfully
    This is working sucessfully , However some code optimisations are
    remaining.

--- popups/sucessfullyInstalled.js
+++ popups/sucessfullyInstalled.js
@@ -1,33 +1,44 @@
+function onStartedDownload(id) {
+  console.log(`Started downloading: ${id}`);
+}
+
+function onFailed(error) {
+  console.log(`Download failed: ${error}`);
+}
+
 function onLinksReceived(downloadLinks) {
-  // console.log(`Started downloading: ${downloadLinks}`);
   var i=0;
+  if (downloadLinks[0]==undefined){
+    console.log("unable to fetch images" );
+  return;
+  }
+
   while(++i<downloadLinks[0].length){
-    
       console.log(downloadLinks[0][i])
-      var downloading = browser.downloads.download({
+      var downloading =this.browser.downloads.download({
         url : downloadLinks[0][i],
         conflictAction : 'uniquify'
       });
+      downloading.then(onStartedDownload, onFailed);
+
     }
  }
   
 function onLinksFailed(error) {
-  console.log(`Download failed: ${error}`);
+  console.log(`Getting Links failed: ${error}`);
 }
 
-document.addEventListener("click", (e) => {
-  
+document.addEventListener("click", (e) => { 
   console.log("in on Click Listner")
   if (e.target.classList.contains("beast")) {
     var chosenBeast = e.target.textContent;
-    console.log('====================================');
+   
    var aray=browser.tabs.executeScript({
       file: "../downloader.js"
     });
 
     aray.then(onLinksReceived, onLinksFailed);
-    console.log(aray);
-     console.log('====================================');
+  
     }
   else if (e.target.classList.contains("clear")) {
     browser.tabs.reload();
