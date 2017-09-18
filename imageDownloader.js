3299903e01633385626228b841e1e1f4387638a2
Rushikesh Tade <rushtade@in.ibm.com>
2017-09-18 17:17:24 +0530

    Submitted for Final Review on mozilla

--- imageDownloader.js
+++ imageDownloader.js
@@ -1,1 +1,59 @@
-console.log("ImageDownloader.js Script");
+
+console.log("Image Downloader Script is executing");
+browser.browserAction.onClicked.addListener(onButtonClickedFunction);
+console.log("Image Downloader Script added listner");
+
+
+function onStartedDownload(id) {
+    console.log(`Started downloading: ${id}`);
+  }
+  
+  function onFailed(error) {
+    console.log(`Download failed: ${error}`);
+  }
+  function onLinksReceived(downloadLinks) {
+    var i=0;
+    if (downloadLinks[0]==undefined){
+      console.log("unable to fetch images" );
+    return;
+    }
+  
+    while(++i<downloadLinks[0].length){
+        console.log(downloadLinks[0][i])
+       var dashSplitString=downloadLinks[0][i].split("/")
+       var fileNameString=dashSplitString[dashSplitString.length-1]
+       var extensionString=fileNameString.split(".")
+       extensionString= extensionString[extensionString.length-1]
+       var fileNameToStore="Google Images"+"/"+i;
+       if ((extensionString=="jpg") ||( extensionString == "bmp") || ( extensionString == "jpeg") ||( extensionString == "gif") || ( extensionString == "png") || ( extensionString == "ico") ||( extensionString == "svg"))
+       {
+        fileNameToStore=fileNameToStore+ "."+ extensionString;
+        console.log("fileNameToStore is"+ fileNameToStore);
+       }
+       else{
+        continue;
+       }
+  
+        var downloading =this.browser.downloads.download({
+          url : downloadLinks[0][i],
+          conflictAction : 'uniquify',
+          filename: fileNameToStore
+        });
+        downloading.then(onStartedDownload, onFailed);
+  
+      }
+   }
+    
+  function onLinksFailed(error) {
+    console.log(`Getting Links failed: ${error}`);
+  }
+  function onButtonClickedFunction(){ 
+    console.log("in on Click Listner")
+    var aray=browser.tabs.executeScript({
+        file: "downloader.js"
+      });
+     aray.then(onLinksReceived, onLinksFailed);
+      return;
+    }
+  
+  
