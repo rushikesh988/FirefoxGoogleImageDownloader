d33ed2a8539f5e1065e19816acd612fa8e62981f
Rushikesh Tade <rushtade@in.ibm.com>
2017-09-19 14:27:32 +0530

    Added Facility for storing in subfolders, Added few more icon sizes in Manifest

--- imageDownloader.js
+++ imageDownloader.js
@@ -1,8 +1,7 @@
 
-console.log("Image Downloader Script is executing");
+console.log("Google Image Downloader Started");
 browser.browserAction.onClicked.addListener(onButtonClickedFunction);
-console.log("Image Downloader Script added listner");
-
+//console.log("Image Downloader Script added listner");
 
 function onStartedDownload(id) {
     console.log(`Started downloading: ${id}`);
@@ -17,18 +16,24 @@ function onStartedDownload(id) {
       console.log("unable to fetch images" );
     return;
     }
-  
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + "-" + today.getMinutes() + "-" + today.getSeconds();
+    var dateTime = date+' '+time;
+
+
+
     while(++i<downloadLinks[0].length){
-        console.log(downloadLinks[0][i])
+        //console.log(downloadLinks[0][i])
        var dashSplitString=downloadLinks[0][i].split("/")
        var fileNameString=dashSplitString[dashSplitString.length-1]
        var extensionString=fileNameString.split(".")
        extensionString= extensionString[extensionString.length-1]
-       var fileNameToStore="Google Images"+"/"+i;
+       var fileNameToStore="Google Images"+"/"+ dateTime+"/"+i;
        if ((extensionString=="jpg") ||( extensionString == "bmp") || ( extensionString == "jpeg") ||( extensionString == "gif") || ( extensionString == "png") || ( extensionString == "ico") ||( extensionString == "svg"))
        {
         fileNameToStore=fileNameToStore+ "."+ extensionString;
-        console.log("fileNameToStore is"+ fileNameToStore);
+        //console.log("fileNameToStore is"+ fileNameToStore);
        }
        else{
         continue;
@@ -48,7 +53,7 @@ function onStartedDownload(id) {
     console.log(`Getting Links failed: ${error}`);
   }
   function onButtonClickedFunction(){ 
-    console.log("in on Click Listner")
+   // console.log("in on Click Listner")
     var aray=browser.tabs.executeScript({
         file: "downloader.js"
       });
