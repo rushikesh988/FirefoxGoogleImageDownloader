c08080db55fa271750e62c25a378b95d32ae2417
Rushikesh Tade <rushtade@in.ibm.com>
2018-02-04 21:37:08 +0530

    Download text file containing links
    - It can now download links to images from file.

--- imageDownloader.js
+++ imageDownloader.js
@@ -1,33 +1,44 @@
 console.log("Google Image Downloader Started");
-var searchTitle="";
+var searchTitle = "";
+var storedSettings = {
+  downloadOption: 0,
+  appendFileNames: false,
+  continuesNumbering: false,
+  AppendComma: false,
+  entryOnNewLine: false
+};
+browser.storage.local.get().then(function (result) {
+  storedSettings = result;
+}, 
+onError);
+
+
+
 browser.browserAction.onClicked.addListener(onButtonClickedFunction);
+
+
 function onStartedDownload(id) {
- // console.log(`Started downloading: ${id}`);
+  // console.log(`Started downloading: ${id}`);
 }
 
 function onError(error) {
   console.log(`Download failed: ${error}`);
 }
+
 function onLinksReceived(downloadLinks) {
-  var storedSettings = {
-    downloadOption: 0,
-    appendFileNames: false,
-    continuesNumbering: false,
-    AppendComma: false,
-    entryOnNewLine: false
-  };
-  var gettingSettings = browser.storage.local.get();
-  gettingSettings.then(function (result) {
+
+  var gettingSettings = browser.storage.local.get().then(function (result) {
     storedSettings = result;
-    console.log(result);
+
   }, function (error) {
     console.log(`Error: ${error}`);
     console.log('Using Default Settings:');
-  
+
   });
-  
- // console.log(storedSettings);
-  
+
+  console.log("Setting Download Option:" + storedSettings.downloadOption);
+  console.log(storedSettings);
+
   if (downloadLinks[0] == undefined) {
     console.log("unable to fetch images");
     return;
@@ -38,9 +49,18 @@ function onLinksReceived(downloadLinks) {
   var dateTime = date + ' ' + time;
 
 
-  var i = 0; 
+  var i = 0;
   var j = 0;
-  if((storedSettings.downloadOption==0)||(storedSettings.downloadOption==2)){
+  var seperatorString = "\n";
+  if (storedSettings.AppendComma) {
+    seperatorString = ",";
+  }
+  if (storedSettings.entryOnNewLine) {
+    seperatorString = seperatorString + "\n";
+  }
+
+  var imgURLsString = "";
+
   while (++i < downloadLinks[0].length) {
     //console.log(downloadLinks[0][i])
     var dashSplitString = downloadLinks[0][i].split("/")
@@ -48,7 +68,7 @@ function onLinksReceived(downloadLinks) {
     //console.log(fileNameString);
     var extensionString = fileNameString.split(".")
     extensionString = extensionString[extensionString.length - 1]
-    var fileNameToStore = "Google Images" + "/" +searchTitle+"_"+ dateTime + "/" + (i - j);
+    var fileNameToStore = "Google Images" + "/" + searchTitle + "_" + dateTime + "/" + (i - j);
     if ((extensionString == "jpg") || (extensionString == "bmp") || (extensionString == "jpeg") || (extensionString == "gif") || (extensionString == "png") || (extensionString == "ico") || (extensionString == "svg")) {
       if (storedSettings.appendFileNames) {
 
@@ -57,37 +77,39 @@ function onLinksReceived(downloadLinks) {
         fileNameToStore = fileNameToStore + "." + extensionString;
 
       }
-
-     console.log("fileNameToStore is"+ fileNameToStore);
-    }
-    else {
+     // console.log("fileNameToStore is" + fileNameToStore);
+    } else {
       if (storedSettings.continuesNumbering) {
         j = j + 1;
-        console.log("Using Continues Numbering: "+ j +" images skipped");
+        console.log("Using Continues Numbering: " + j + " images skipped");
       }
       continue;
     }
+    imgURLsString = imgURLsString + downloadLinks[0][i] + seperatorString;
+    if ((storedSettings.downloadOption == 0) || (storedSettings.downloadOption == 2)) {
+      var downloading = this.browser.downloads.download({
+        url: downloadLinks[0][i],
+        conflictAction: 'uniquify',
+        filename: fileNameToStore
+      });
+      downloading.then(onStartedDownload, onError);
+
+    }
+  }
+
+  if ((storedSettings.downloadOption == 1) || (storedSettings.downloadOption == 2)) {
+    var URLsFileName = "Google Images" + "/" + searchTitle + "_" + dateTime + "/" + "Links.txt";
+    imgURLsString = imgURLsString.substring(0, imgURLsString.length - seperatorString.length);
+    var blob = new Blob([imgURLsString], {
+      type: 'text/plain'
+    });
 
     var downloading = this.browser.downloads.download({
-      url: downloadLinks[0][i],
-      conflictAction: 'uniquify',
-      filename: fileNameToStore
+      filename: URLsFileName,
+      url: URL.createObjectURL(blob),
+      conflictAction: 'uniquify'
     });
     downloading.then(onStartedDownload, onError);
-
-  }}
-
-  if((storedSettings.downloadOption==1)||(storedSettings.downloadOption==2)){
-    i=0;
-
-    var seperatorString="\n";
-    if(storedSettings.AppendComma){
-      seperatorString=",";
-    }
-    if(storedSettings.entryOnNewLine){
-      seperatorString=seperatorString+"\n";
-    }
-
 
 
   }
@@ -97,22 +119,24 @@ function onLinksReceived(downloadLinks) {
 
 
 function onButtonClickedFunction() {
-  browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT})
-  .then(tabs => browser.tabs.get(tabs[0].id))
-  .then(tab => {
-    console.log(tab.title);
-    searchTitle=  tab.title.split('-')[0]
+  browser.tabs.query({
+      active: true,
+      windowId: browser.windows.WINDOW_ID_CURRENT
+    })
+    .then(tabs => browser.tabs.get(tabs[0].id))
+    .then(tab => {
+      console.log(tab.title);
+      searchTitle = tab.title.split('-')[0]
 
-  console.log("in on Click Listner")
-  var aray = browser.tabs.executeScript({
-    file: "downloader.js"
-  });
-  aray.then(onLinksReceived, onError);
+      console.log("in on Click Listner")
+      var aray = browser.tabs.executeScript({
+        file: "downloader.js"
+      });
+      aray.then(onLinksReceived, onError);
 
-  });
+    });
 
 
- 
+
   return;
 }
-
